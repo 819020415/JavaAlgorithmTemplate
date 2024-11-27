3a72c12f856ace0699c03e7ae75b21fba111b0bb
819020415 <52266181+819020415@users.noreply.github.com>
2024-11-27 13:41:57 +0800

    Add files via upload

--- algorithm/质数筛/欧拉筛.java
+++ algorithm/质数筛/欧拉筛.java
@@ -2,13 +2,14 @@ package algorithm.质数筛;
 
 public class 欧拉筛 {
     //欧式筛，线性筛
-    static  int[] p;
-    static boolean[] st ;//st[i]=true 表示 i 是质数
+    static int[] p;
+    static boolean[] st;//st[i] = true 表示 i 不是质数
+    static int cnt;
 
     static void f(int n) {
-        int cnt = 0;
         p = new int[n + 1];
-        st = new boolean[n + 1];//st[i]=true 表示 i 是质数
+        st = new boolean[n + 1];//st[i] = true 表示 i 不是质数
+        st[0] = st[1] = true;
         for (int i = 2; i <= n; i++) {
             if (!st[i]) p[cnt++] = i;
             for (int j = 0; p[j] * i <= n; j++) {
@@ -19,7 +20,4 @@ public class 欧拉筛 {
     }
 
 
-    public static void main(String[] args) {
-        f(100000);
-    }
 }
